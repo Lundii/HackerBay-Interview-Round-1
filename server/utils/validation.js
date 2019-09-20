1cd18db308fe6dd759cff82a95fa76a322cd989e
Lundii <mondayemmanuel67@gmail.com>
2019-09-20 18:36:40 +0100

    login validation

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,5 +1,11 @@
 import { body, validationResult } from 'express-validator';
 
+/**
+ * validation middleware to check to validate input paraments
+ * @param {object} req - the request object
+ * @param {object} res - the response object
+ * @param {function} next - function to call the next middleware
+ */
 export const validate = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -9,7 +15,11 @@ export const validate = (req, res, next) => {
   next();
 };
 
+/**
+ * Array of chained input validations to check the passed username and password
+ */
 export const loginValidation = [
   body('username', 'username is required').exists(),
+  body('username', 'username must be a string').isString(),
   body('password', 'password is required').exists(),
 ];
